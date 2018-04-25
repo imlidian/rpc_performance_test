93fc808f3c01c095c4fe810e8378c75cbff7a3dc
DeanLee <lidian1@huawei.com>
2018-04-25 15:58:14 +0800

    demo-pojo client and shell scrpit

--- demo-pojo/deploy.sh
+++ demo-pojo/deploy.sh
@@ -4,9 +4,9 @@ UP="u"
 INSTALL="i"
 ALL="a"
 #export PATH1="/Volumes/256G/imlidian/chassis/incubator-servicecomb-java-chassis"
-export PATH1="/Volumes/256G/imlidian/Chasiss"
-export SJAR="pojo-server-1.0.0-m2-SNAPSHOT.jar"
-export CJAR="pojo-client-1.0.0-m2-SNAPSHOT.jar"
+export PATH1="/Volumes/256G/imlidian/test/demo-pojo"
+export SJAR="servicecomb-pojo-server-round-2.jar"
+export CJAR="servicecomb-pojo-client-round-2.jar"
 
 #/Volumes/256G/imlidian/Chasiss/demo/demo-pojo
 
@@ -18,10 +18,11 @@ if [ $INSTALL = "1" -o $ALL = "a" ];then
 #rm -rf /Users/dean/.m2/repository/org/apache/servicecomb/demo/pojo-client
 #rm -rf /Users/dean/.m2/repository/org/apache/servicecomb/demo/pojo-server
 #rm -rf $PATH1/demo/demo-pojo/benchmark-base/target/benchmark-base-round-1.jar
-rm -rf $PATH1/demo/target/Java\ Chassis\:\:Demo\:\:POJO\:\:Client/$CJAR
-rm -rf $PATH1/demo/target/Java\ Chassis\:\:Demo\:\:POJO\:\:Server/$SJAR
-cd $PATH1/demo/demo-pojo
-mvn clean install -Pdemo-run-release
+#rm -rf $PATH1/demo/target/Java\ Chassis\:\:Demo\:\:POJO\:\:Client/$CJAR
+#rm -rf $PATH1/demo/target/Java\ Chassis\:\:Demo\:\:POJO\:\:Server/$SJAR
+cd $PATH1/demo-pojo
+#mvn clean install -Pdemo-run-release
+mvn clean install
 fi
 
 if [ $D = "d" -o $ALL = "a" ];then
@@ -29,8 +30,6 @@ ssh root@hwc2 "cd java ; ls ;rm $CJAR ; killall java"
 ssh root@hws "cd java ; ls ;rm $SJAR ; killall Java"
 fi
 if [ $UP = "u" -o $ALL = "a" ];then
-scp $PATH1/demo/target/Java\ Chassis\:\:Demo\:\:POJO\:\:Client/$CJAR root@hwc2:~/java  #hwc2
-scp $PATH1/demo/target/Java\ Chassis\:\:Demo\:\:POJO\:\:Server/$SJAR root@hws:~/java  #hws
-scp $PATH1/demo/demo-pojo/benchmark-base/target/benchmark-base-round-1.jar root@hwc2:~/java
-scp $PATH1/demo/demo-pojo/benchmark-base/target/benchmark-base-round-1.jar root@hws:~/java
+scp $PATH1/pojo-client/target/$CJAR root@hwc2:~/java  #hwc2
+scp $PATH1/pojo-server/target/$SJAR root@hws:~/java  #hws
 fi
